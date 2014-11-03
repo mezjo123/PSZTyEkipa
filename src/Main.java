046fbdcd10d2e61f4a72ab2d110ffb1142c24c43
Mez <matmaz180@gmail.com>
2014-11-03 20:37:26 +0100

    Beginnings

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * Created by Marzuz on 2014-11-03.
+ */
+public class Main {
+
+    public int x;
+
+    public static void main(String[] args) {
+        System.out.println("testst");
+        Okno okno = new Okno();
+    }
+}
---
+++ src/Okno.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * NIe
+ * Created by Marzuz on 2014-11-03.
+ */
+public class Okno {
+    public Okno(){
+        JFrame okno = new JFrame("Test");
+        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okno.setSize(300,100);
+        okno.setVisible(true);
+    }
+}
